4d735f50627637eb433cd896d604503bab8eebcc
MouVen <MouVen@users.noreply.github.com>
2023-08-13 06:31:08 +0530

    Update solution.sv

--- Problems/p000 Hello World/solution.sv
+++ Problems/p000 Hello World/solution.sv
@@ -1,5 +1,8 @@
-//Problem Description: <link to blogger>
+//Problem Description: https://mouven.blogspot.com/2023/08/system-verilog-hello-world.html
 class solution;
-  	function void your_solution(output string s);
+
+    function void your_solution(output string s);
+    //Type your Solution Here
     endfunction
+
 endclass
